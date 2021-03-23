968145b614555ef48245cd8367568dc09093e48e
maneewihok <tanakit.wihok@gmail.com>
2021-03-23 10:35:37 +0700

    edit flex score in card

--- components/Home.vue
+++ components/Home.vue
@@ -146,14 +146,14 @@
               <img class="relative w-40" :src="recom.image_url" />
             </div>
             <div class="flex justify-start relative text-white px-6 pb-6">
-              <div class="fix-sizex flex">
+              <div class="fix-sizex">
                 <span
                   @click="RecomClick(index), $router.push('/detail')"
                   class="cursor-pointer hover:text-orange block font-semibold text-xl mt-10"
                 >
                   {{ recom.title }}
                 </span>
-                <div class="flex justify-start">
+                <div class="flex">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow" fill="currentColor">
                     <path
                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -197,13 +197,13 @@
               <img class="relative w-40" :src="manga.image_url" />
             </div>
             <div class="relative text-white px-6 pb-6">
-              <div class="fix-sizex flex justify-start">
+              <div class="fix-sizex">
                 <span
                   @click="MangaClick(mal_id), $router.push('/detail')"
                   class="cursor-pointer hover:text-orange block font-semibold text-xl mt-10"
                   >{{ manga.title }}</span
                 >
-                <div class="flex justify-start">
+                <div class="flex">
                   <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-yellow" fill="currentColor">
                     <path
                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"

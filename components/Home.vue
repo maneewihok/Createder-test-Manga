70898bc8fe8b5944dde3744c8a4f96ec0b38dcec
maneewihok <tanakit.wihok@gmail.com>
2021-03-25 01:03:19 +0700

    edit cesign search bar

--- components/Home.vue
+++ components/Home.vue
@@ -86,38 +86,45 @@
       </div>
     </div>
     <!--search--->
-    <div v-show="showSearch" class="pt-2 relative mx-auto text-gray-600">
-      <input
-        @keyup.enter="search($event)"
-        v-model="query"
-        class="border-2 border-gray bg-white w-full h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-        type="search"
-        name="search"
-        placeholder="Search"
-      />
-      <button @click="search" type="submit" class="absolute right-0 top-0 mt-5 mr-4">
-        <svg
-          class="text-gray-600 h-4 w-4 fill-current"
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          version="1.1"
-          id="Capa_1"
-          x="0px"
-          y="0px"
-          viewBox="0 0 56.966 56.966"
-          style="enable-background: new 0 0 56.966 56.966"
-          xml:space="preserve"
-          width="512px"
-          height="512px"
+    <div v-show="showSearch" class="flex justify-end pt-2 relative mx-auto bg-black mt-3 rounded-full">
+      <div class="flex mr-3">
+        <input
+          @keyup.enter="search($event)"
+          v-model="query"
+          class="bg-white w-30 h-10 px-5 pr-16 rounded-full rounded-r text-sm focus:outline-none ml-3 mb-3"
+          type="search"
+          name="search"
+          placeholder="Search"
+        />
+        <button
+          @click="search"
+          type="submit"
+          class="bg-white right-0 top-0 h-10 w-10 rounded-full rounded-l border-l border-gray"
         >
-          <path
-            d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
-          />
-        </svg>
-      </button>
-      <div class="ml-3 mt-3 text-danger">Ex.Dragon ball , One Piece ...</div>
+          <svg
+            class="text-black h-4 w-4 fill-current ml-2"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="Capa_1"
+            x="0px"
+            y="0px"
+            viewBox="0 0 56.966 56.966"
+            style="enable-background: new 0 0 56.966 56.966"
+            xml:space="preserve"
+            width="512px"
+            height="512px"
+          >
+            <path
+              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+            />
+          </svg>
+        </button>
+      </div>
     </div>
+    <div class="flex justify-end ml-3 mt-3 mr-3 text-danger">Ex.Dragon ball , One Piece ...</div>
     <client-only>
+      <div>แนะนำ</div>
       <sequential-entrance delay="1000">
         <div v-show="showReccom" class="p-24 flex flex-wrap items-center justify-center">
           <div
@@ -164,7 +171,6 @@
               </div>
             </div>
           </div>
-          <div class="bg-danger text-xl text-white w-20 text-center rounded">แนะนำ</div>
         </div>
       </sequential-entrance>
     </client-only>
@@ -258,7 +264,7 @@ export default {
     }
   },
   async fetch() {
-    const url = await this.$http.$get(`https://api.jikan.moe/v3/search/anime?q=onepiech&limit=3`)
+    const url = await this.$http.$get(`https://api.jikan.moe/v3/search/anime?q=onepiech&limit=6`)
     this.recom = url.results
   },
   methods: {
